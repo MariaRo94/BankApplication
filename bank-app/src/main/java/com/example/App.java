8d8b8ae17ec5d7c54d1402bea3ac9a120b54eb00
Maria Rodionova <mrodionova@aflt-systems.ru>
2025-10-16 15:40:47 +0300

    Main class added

---
+++ bank-app/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.example");
+        context.getBean(OperationConsooleListener.class)
+                .listenUpdate();
+    }
+}
